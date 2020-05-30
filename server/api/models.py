309ebd24234b5890bc3c6b3ffaef8834e8cfb02c
freedev111 <freedev111@outlook.com>
2020-05-30 14:16:29 -0400

    created jbo table

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+  title = models.CharField(max_length=20, blank=True, null=True)
+  location = models.CharField(max_length=20, blank=True, null=True)
+  title_id = models.CharField(max_length=50, blank=True, null=True)
+  normalized_title = models.CharField(max_length=20, blank=True, null=True)
+
+  def __str__(self):
+    return self.normalized_title
